143c6753b685f0f76283763c90a952106a0f1952
David-Irving <djirving84@gmail.com>
2022-05-19 17:19:07 -0600

    working code

--- db/db.sql
+++
@@ -1,4 +0,0 @@
-drop database if exists employeetracker;
-create database employeetracker;
-
-use employeetracker;
--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,31 @@
+drop database if exists employeetracker;
+create database employeetracker;
+use employeetracker;
+
 drop table if exists department;
 drop table if exists employee;
 drop table if exists roles;
 
 create table department (
     id int primary key auto_increment,
-    name varchar(30) 
+    name varchar(30) not null
 );
 
-create table roles (
+create table role (
     id int primary key auto_increment,
-    title varchar(30),
-    salary decimal,
+    title varchar(30) not null,
+    salary decimal not null,
     department_id int,
-    constraint dptfk foreign key (department_id) references department(id)
+    constraint dptfk foreign key (department_id) references department(id) on delete set null
 );
 
 create table employee (
     id int primary key auto_increment,
-    first_name varchar(30),
-    last_name varchar(30),
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
     role_id int,
-    constraint roleID foreign key (role_id) references roles(id),
+    constraint roleID foreign key (role_id) references roles(id) on delete set null,
     manager_id int,
-    constraint manager foreign key (manager_id) references employee(id)
+    constraint manager foreign key (manager_id) references employee(id) on delete set null
 );
 
